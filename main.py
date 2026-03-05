773fb3b1882943f2c8a07cac429b8966f0c28e16
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-03-05 07:32:28 -0600

    implementación dfs

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from utils.load_data import load_costs, load_heuristics
+from algorithms.bfs import bfs
+from algorithms.dfs import dfs
 
-heuristica = load_heuristics('./data/heuristica.xlsx')
 costos = load_costs('./data/funcion_de_costo.xlsx')
+solucion = dfs(costos, "Warm-up activities", "Stretching")
 
-print(heuristica)
-print(costos)
+if solucion:
+    print('camino encontrado')
+    print(solucion.path())
+    print('Acciones:')
+    print(solucion.solution())
+else:
+    print('No hay solución')
+
